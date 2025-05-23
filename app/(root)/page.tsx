5544d017aba933f6be5ae451d5a68d77ad6815d1
Gustavo Avide <g_a93@outlook.com>
2025-05-23 22:32:06 +0100

    feat: add loading page and not found page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export const metadata = {
   title: "Home ",
 };
 
-const Homepage = () => {
+const Homepage = async () => {
+  await delay(1000);
   return;
 };
 
