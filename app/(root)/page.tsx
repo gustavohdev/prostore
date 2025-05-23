78c8bb31d853bba8a449f878b58a4c94cdbfe960
Gustavo Avide <g_a93@outlook.com>
2025-05-23 13:16:04 +0100

    feat: add footer and header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,8 @@ export const metadata = {
   title: "Home ",
 };
 
-import { Button } from "@/components/ui/button";
-
 const Homepage = () => {
-  return <Button>Button</Button>;
+  return;
 };
 
 export default Homepage;
