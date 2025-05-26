db0bad9df2cc7224c63f197fe9940440c4bd8ac5
Gustavo Avide <g_a93@outlook.com>
2025-05-26 14:07:15 +0100

    feat: add productlist component, add sampledb data, add sheet component to smaller screens

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const metadata = {
   title: "Home ",
 };
 
 const Homepage = async () => {
   await delay(1000);
-  return;
+  console.log(sampleData);
+  return (
+    <>
+      ProStore
+      <ProductList
+        data={sampleData.products}
+        title="Featured Products"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -23,19 +21,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart className="h-5 w-5" /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon className="h-5 w-5" /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
