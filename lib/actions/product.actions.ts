69a711674e8d45f74a72fa5f28eea02f93e3a01d
Gustavo Avide <g_a93@outlook.com>
2025-05-29 18:54:52 +0100

    feat: websockets not activated and details page add

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -16,3 +16,15 @@ export async function getLatestProducts() {
 
   return convertToPlainObject(data);
 }
+
+// Get single product by slug
+
+export async function getProductBySlug(slug: string) {
+  const data = await prisma.product.findFirst({
+    where: {
+      slug,
+    },
+  });
+
+  return convertToPlainObject(data);
+}
